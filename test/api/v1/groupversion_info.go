be60e15e674e10b00e88f49cf03aab46b5d01f41
Charith Ellawala <52399125+charith-elastic@users.noreply.github.com>
2020-05-15 14:28:15 +0100

    Sort kinds (#4)
    * Add method to get kinds sorted
    
    * Add test script
    
    * Simplify sort kinds

---
+++ test/api/v1/groupversion_info.go
@@ -0,0 +1,37 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// Package v1 contains API Schema definitions for the webapp v1 API group
+// +kubebuilder:object:generate=true
+// +groupName=webapp.test.k8s.elastic.co
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "webapp.test.k8s.elastic.co", Version: "v1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
---
+++ test/api/v1/guestbook_types.go
@@ -0,0 +1,71 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GuestbookSpec defines the desired state of Guestbook.
+type GuestbookSpec struct {
+	// Page indicates the page number
+	Page *int `json:"page,omitempty"`
+	// Entries contain guest book entries for the page
+	Entries []GuestbookEntry `json:"entries,omitempty"`
+	// Selector selects something
+	Selector metav1.LabelSelector `json:"selector,omitempty"`
+}
+
+// GuestbookEntry defines an entry in a guest book.
+type GuestbookEntry struct {
+	// Name of the guest
+	Name string `json:"name,omitempty"`
+	// Time of entry
+	Time metav1.Time `json:"time,omitempty"`
+	// Comment by guest
+	Comment string `json:"comment,omitempty"`
+}
+
+// GuestbookStatus defines the observed state of Guestbook.
+type GuestbookStatus struct {
+	Status string `json:"status"`
+}
+
+// +kubebuilder:object:root=true
+
+// Guestbook is the Schema for the guestbooks API.
+type Guestbook struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GuestbookSpec   `json:"spec,omitempty"`
+	Status GuestbookStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// GuestbookList contains a list of Guestbook.
+type GuestbookList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Guestbook `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Guestbook{}, &GuestbookList{})
+}
