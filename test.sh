a5caad21e2fcf265ce4c876a28cff4bb906fa12f
Reinhard Nägele <unguiculus@gmail.com>
2021-10-20 17:03:00 +0200

    Re-use variable
    Co-authored-by: Thibault Richard <thbkrkr@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -24,7 +24,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TEMP_DIR=$(mktemp -d -t crd-ref-docs-XXXXX)
 ACTUAL="${TEMP_DIR}/out.asciidoc"
-DEFAULT_ARGS=(--log-level=ERROR --source-path="${SCRIPT_DIR}/test" --renderer=asciidoctor --output-path="${TEMP_DIR}/out.asciidoc")
+DEFAULT_ARGS=(--log-level=ERROR --source-path="${SCRIPT_DIR}/test" --renderer=asciidoctor --output-path="${ACTUAL}")
 
 trap '[[ $TEMP_DIR ]] && rm -rf "$TEMP_DIR"' EXIT
 
