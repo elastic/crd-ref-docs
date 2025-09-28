1a66dc192e9a0af0f063f9e73663ccc8363760f3
Eng Zer Jun <engzerjun@gmail.com>
2025-09-29 00:57:23 +0800

    fix(deps): update module github.com/Masterminds/sprig/v3 to v3.3.0
    Spring v3 was released in October 2019. v3 introduced edge case behavior
    changes with semver.
    
    Reference: https://github.com/Masterminds/sprig/releases/tag/v3.0.0
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- renderer/markdown.go
+++ renderer/markdown.go
@@ -23,7 +23,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/Masterminds/sprig"
+	"github.com/Masterminds/sprig/v3"
 	"github.com/elastic/crd-ref-docs/config"
 	"github.com/elastic/crd-ref-docs/templates"
 	"github.com/elastic/crd-ref-docs/types"
